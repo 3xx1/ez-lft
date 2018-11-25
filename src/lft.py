a02131139eda95f4f5d8a9abaea8406926a0fa41
Koji Abe <Koji@Koji-Abe-no-MacBook.local>
2018-11-25 10:30:39 -0800

    code modified, but failed

--- src/lft.py
+++ src/lft.py
@@ -1,7 +1,20 @@
 import matplotlib.pyplot as plt
+import numpy as np
 import cv2
+from matplotlib.widgets  import RectangleSelector
 
 I = cv2.imread('images/LFT_example.png')
 
 plt.imshow(cv2.cvtColor(I, cv2.COLOR_BGR2RGB))
 plt.show()
+
+def line_select_callback(eclick, erelease):
+    x1, y1 = eclick.xdata, eclick.ydata
+    x2, y2 = erelease.xdata, erelease.ydata
+
+rs = RectangleSelector(I, line_select_callback,
+                       drawtype='box', useblit=False, button=[1],
+                       minspanx=5, minspany=5, spancoords='pixels',
+                       interactive=True)
+
+rs.mean()
