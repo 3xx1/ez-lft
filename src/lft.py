804c18a439c41437fc8ed87cf8117388cf15dfbb
Koji Abe <Koji@Koji-Abe-no-MacBook.local>
2018-11-27 14:24:00 -0800

    conflict fixed

--- src/lft.py
+++ src/lft.py
@@ -4,26 +4,76 @@ import cv2
 from matplotlib.widgets  import RectangleSelector
 import matplotlib
 
-img_source = cv2.imread('images/LFT_example.png')
-# img_grayscale = cv2.cvtColor(img_source, cv2.COLOR_BGR2GRAY)
-# _, img_binary = cv2.threshold(img_grayscale, 250, 255, cv2.THRESH_BINARY)
-# spectrumContainers, _ = cv2.findContours(img_binary, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-# print(len(spectrumContainers))
-#
-# # I = cv2.cvtColor(I, cv2.COLOR_BGR2RGB)
 
-plt.imshow(cv2.cvtColor(img_source, cv2.COLOR_BGR2RGB))
+I = cv2.imread('images/LFT_example.png')
+
+ROI_control = I[203:223, 190:270]
+
+ROI_test = I[378:398, 190:270]
+
+ROI_background = I[418:438, 190:270]
+
+b_ROI_control = ROI_control.copy()
+# set green and red channels to 0
+b_ROI_control[:, :, 1] = 0
+b_ROI_control[:, :, 2] = 0
+
+g_ROI_control = ROI_control.copy()
+# set blue and red channels to 0
+g_ROI_control[:, :, 0] = 0
+g_ROI_control[:, :, 2] = 0
+
+r_ROI_control = ROI_control.copy()
+# set blue and green channels to 0
+r_ROI_control[:, :, 0] = 0
+r_ROI_control[:, :, 1] = 0
+
+b_ROI_test = ROI_test.copy()
+# set green and red channels to 0
+b_ROI_test[:, :, 1] = 0
+b_ROI_test[:, :, 2] = 0
+
+g_ROI_test = ROI_test.copy()
+# set blue and red channels to 0
+g_ROI_test[:, :, 0] = 0
+g_ROI_test[:, :, 2] = 0
+
+r_ROI_test = ROI_test.copy()
+# set blue and green channels to 0
+r_ROI_test[:, :, 0] = 0
+r_ROI_test[:, :, 1] = 0
+
+b_ROI_background = ROI_background.copy()
+# set green and red channels to 0
+b_ROI_background[:, :, 1] = 0
+b_ROI_background[:, :, 2] = 0
+
+g_ROI_background = ROI_background.copy()
+# set blue and red channels to 0
+g_ROI_background[:, :, 0] = 0
+g_ROI_background[:, :, 2] = 0
+
+r_ROI_background = ROI_background.copy()
+# set blue and green channels to 0
+r_ROI_background[:, :, 0] = 0
+r_ROI_background[:, :, 1] = 0
+
+roi_control_avg_intensity = np.mean(g_ROI_control)
+
+roi_test_avg_intensity = np.mean(g_ROI_test)
+
+roi_background_avg_intensity = np.mean(g_ROI_background)
+
+roi_corrected = (roi_test_avg_intensity - roi_background_avg_intensity)/(0 - roi_background_avg_intensity)
+
+print roi_corrected
+print roi_control_avg_intensity
+print roi_test_avg_intensity
+print roi_background_avg_intensity
+
+ROI_control_box = cv2.rectangle(I,(190,203),(270,223),(255,0,0),3)
+ROI_test_box = cv2.rectangle(I,(190,378),(270,398),(0,0,255),3)
+ROI_background_box = cv2.rectangle(I,(190,418),(270,438),(0,0,0),3)
+
+plt.imshow(cv2.cvtColor(I, cv2.COLOR_BGR2RGB))
 plt.show()
-
-# def line_select_callback(eclick, erelease):
-#     x1, y1 = eclick.xdata, eclick.ydata
-#     x2, y2 = erelease.xdata, erelease.ydata
-#
-# rs = RectangleSelector(I, line_select_callback,
-#                        drawtype='box', useblit=False, button=[1],
-#                        minspanx=5, minspany=5, spancoords='pixels',
-#                        interactive=True)
-
-# rs.mean()
-
-# print(rs)
