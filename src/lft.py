9d08ee335b2cd77f9b9bf17fe4796e312b497611
Koji Abe <Koji@Koji-Abe-no-MacBook.local>
2018-11-28 12:14:34 -0800

    modified code success

--- src/lft.py
+++ src/lft.py
@@ -76,13 +76,7 @@ r_ROI_background_crop = ROI_background_crop.copy()
 r_ROI_background_crop[:, :, 0] = 0
 r_ROI_background_crop[:, :, 1] = 0
 
-<<<<<<< HEAD
 roi_control_avg_intensity = np.mean(g_ROI_control_crop)
-=======
-roi_control_avg_intensity = np.mean(g_ROI_control)
-
-roi_test_avg_intensity = np.mean(g_ROI_test)
->>>>>>> modified code success
 
 roi_test_avg_intensity = np.mean(g_ROI_test_crop)
 
@@ -95,15 +89,9 @@ print roi_control_avg_intensity
 print roi_test_avg_intensity
 print roi_background_avg_intensity
 
-<<<<<<< HEAD
 ROI_control_box = cv2.rectangle(I,(int(ROI_control[0]),int(ROI_control[1])),(int(ROI_control[0]+ROI_control[2]),int(ROI_control[1]+ROI_control[3])),(255,0,0),3)
 ROI_test_box = cv2.rectangle(I,(int(ROI_control[0]),int(ROI_control[1])+Distance_control_test),(int(ROI_control[0]+ROI_control[2]),int(ROI_control[1]+ROI_control[3])+Distance_control_test),(0,0,255),3)
 ROI_background_box = cv2.rectangle(I,(int(ROI_control[0]),int(ROI_control[1])+Distance_control_background),(int(ROI_control[0]+ROI_control[2]),int(ROI_control[1]+ROI_control[3])+Distance_control_background),(0,0,0),3)
-=======
-ROI_control_box = cv2.rectangle(I,(190,203),(270,223),(255,0,0),3)
-ROI_test_box = cv2.rectangle(I,(190,378),(270,398),(0,0,255),3)
-ROI_background_box = cv2.rectangle(I,(190,418),(270,438),(0,0,0),3)
->>>>>>> modified code success
 
 plt.imshow(cv2.cvtColor(I, cv2.COLOR_BGR2RGB))
 plt.show()
