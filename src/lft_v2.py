b1334cfe7dcd398b3ddb5a2aec8e40880dce529b
Koji Abe <Koji@Koji-Abe-no-MacBook.local>
2018-11-29 22:25:04 -0800

    modifying

--- src/lft_v2.py
+++ src/lft_v2.py
@@ -8,7 +8,8 @@ import matplotlib
 Distance_control_test = 175
 Distance_control_background = 215
 
-I = cv2.imread('images/LFT_example.png')
+I_raw = cv2.imread('images/LFT_example.png')
+I = cv2.resize(I_raw, (700, 700))
 
 # Select ROI_control
 fromCenter = False
@@ -19,86 +20,86 @@ ROI_control_crop = I[int(ROI_control[1]):int(ROI_control[1]+ROI_control[3]), int
 ROI_test_crop = I[int(ROI_control[1])+Distance_control_test:int(ROI_control[1]+ROI_control[3]+Distance_control_test), int(ROI_control[0]):int(ROI_control[0]+ROI_control[2])]
 ROI_background_crop = I[int(ROI_control[1])+Distance_control_background:int(ROI_control[1]+ROI_control[3]+Distance_control_background), int(ROI_control[0]):int(ROI_control[0]+ROI_control[2])]
 
-#ROI_control = I[203:223, 190:270]
+##ROI_control = I[203:223, 190:270]
 
-#ROI_test = I[378:398, 190:270]
+##ROI_test = I[378:398, 190:270]
 
-#ROI_background = I[418:438, 190:270]
-
-
-b_ROI_control_crop = ROI_control_crop.copy()
-# set green and red channels to 0
-b_ROI_control_crop[:, :, 1] = 0
-b_ROI_control_crop[:, :, 2] = 0
-
-g_ROI_control_crop = ROI_control_crop.copy()
-# set green and red channels to 0
-g_ROI_control_crop[:, :, 0] = 0
-g_ROI_control_crop[:, :, 2] = 0
-
-r_ROI_control_crop = ROI_control_crop.copy()
-# set green and red channels to 0
-r_ROI_control_crop[:, :, 0] = 0
-r_ROI_control_crop[:, :, 1] = 0
+##ROI_background = I[418:438, 190:270]
 
 
+#b_ROI_control_crop = ROI_control_crop.copy()
+## set green and red channels to 0
+#b_ROI_control_crop[:, :, 1] = 0
+#b_ROI_control_crop[:, :, 2] = 0
 
-b_ROI_test_crop = ROI_test_crop.copy()
-# set green and red channels to 0
-b_ROI_test_crop[:, :, 1] = 0
-b_ROI_test_crop[:, :, 2] = 0
+#g_ROI_control_crop = ROI_control_crop.copy()
+## set green and red channels to 0
+#g_ROI_control_crop[:, :, 0] = 0
+#g_ROI_control_crop[:, :, 2] = 0
 
-g_ROI_test_crop = ROI_test_crop.copy()
-# set blue and red channels to 0
-g_ROI_test_crop[:, :, 0] = 0
-g_ROI_test_crop[:, :, 2] = 0
-
-r_ROI_test_crop = ROI_test_crop.copy()
-# set blue and green channels to 0
-r_ROI_test_crop[:, :, 0] = 0
-r_ROI_test_crop[:, :, 1] = 0
+#r_ROI_control_crop = ROI_control_crop.copy()
+## set green and red channels to 0
+#r_ROI_control_crop[:, :, 0] = 0
+#r_ROI_control_crop[:, :, 1] = 0
 
 
 
+#b_ROI_test_crop = ROI_test_crop.copy()
+## set green and red channels to 0
+#b_ROI_test_crop[:, :, 1] = 0
+#b_ROI_test_crop[:, :, 2] = 0
 
-b_ROI_background_crop = ROI_background_crop.copy()
-# set green and red channels to 0
-b_ROI_background_crop[:, :, 1] = 0
-b_ROI_background_crop[:, :, 2] = 0
+#g_ROI_test_crop = ROI_test_crop.copy()
+## set blue and red channels to 0
+#g_ROI_test_crop[:, :, 0] = 0
+#g_ROI_test_crop[:, :, 2] = 0
 
-g_ROI_background_crop = ROI_background_crop.copy()
-# set blue and red channels to 0
-g_ROI_background_crop[:, :, 0] = 0
-g_ROI_background_crop[:, :, 2] = 0
+#r_ROI_test_crop = ROI_test_crop.copy()
+## set blue and green channels to 0
+#r_ROI_test_crop[:, :, 0] = 0
+#r_ROI_test_crop[:, :, 1] = 0
 
-r_ROI_background_crop = ROI_background_crop.copy()
-# set blue and green channels to 0
-r_ROI_background_crop[:, :, 0] = 0
-r_ROI_background_crop[:, :, 1] = 0
 
-roi_control_avg_intensity = np.mean(g_ROI_control_crop)
 
-roi_test_avg_intensity = np.mean(g_ROI_test_crop)
 
-roi_background_avg_intensity = np.mean(g_ROI_background_crop)
+#b_ROI_background_crop = ROI_background_crop.copy()
+## set green and red channels to 0
+#b_ROI_background_crop[:, :, 1] = 0
+#b_ROI_background_crop[:, :, 2] = 0
 
-roi_corrected_0to1 = (roi_test_avg_intensity - roi_background_avg_intensity)/(0 - roi_background_avg_intensity)
+#g_ROI_background_crop = ROI_background_crop.copy()
+## set blue and red channels to 0
+#g_ROI_background_crop[:, :, 0] = 0
+#g_ROI_background_crop[:, :, 2] = 0
 
-print roi_corrected_0to1
-print roi_control_avg_intensity
-print roi_test_avg_intensity
-print roi_background_avg_intensity
+#r_ROI_background_crop = ROI_background_crop.copy()
+## set blue and green channels to 0
+#r_ROI_background_crop[:, :, 0] = 0
+#r_ROI_background_crop[:, :, 1] = 0
 
-# output to text file
-lines = ['roi_corrected_0to1', str(roi_corrected_0to1), 'roi_control_avg_intensity', str(roi_control_avg_intensity), 'roi_test_avg_intensity', str(roi_test_avg_intensity), 'roi_background_avg_intensity', str(roi_background_avg_intensity)]
-with open('output.txt', 'w') as file:
-    file.write('\n'.join(lines))
+#roi_control_avg_intensity = np.mean(g_ROI_control_crop)
 
-file.close()
+#roi_test_avg_intensity = np.mean(g_ROI_test_crop)
 
-ROI_control_box = cv2.rectangle(I,(int(ROI_control[0]),int(ROI_control[1])),(int(ROI_control[0]+ROI_control[2]),int(ROI_control[1]+ROI_control[3])),(255,0,0),3)
-ROI_test_box = cv2.rectangle(I,(int(ROI_control[0]),int(ROI_control[1])+Distance_control_test),(int(ROI_control[0]+ROI_control[2]),int(ROI_control[1]+ROI_control[3])+Distance_control_test),(0,0,255),3)
-ROI_background_box = cv2.rectangle(I,(int(ROI_control[0]),int(ROI_control[1])+Distance_control_background),(int(ROI_control[0]+ROI_control[2]),int(ROI_control[1]+ROI_control[3])+Distance_control_background),(0,0,0),3)
+#roi_background_avg_intensity = np.mean(g_ROI_background_crop)
 
-plt.imshow(cv2.cvtColor(I, cv2.COLOR_BGR2RGB))
-plt.show()
+#roi_corrected_0to1 = (roi_test_avg_intensity - roi_background_avg_intensity)/(0 - roi_background_avg_intensity)
+
+#print roi_corrected_0to1
+#print roi_control_avg_intensity
+#print roi_test_avg_intensity
+#print roi_background_avg_intensity
+
+## output to text file
+#lines = ['roi_corrected_0to1', str(roi_corrected_0to1), 'roi_control_avg_intensity', str(roi_control_avg_intensity), 'roi_test_avg_intensity', str(roi_test_avg_intensity), 'roi_background_avg_intensity', str(roi_background_avg_intensity)]
+#with open('output.txt', 'w') as file:
+#    file.write('\n'.join(lines))
+
+#file.close()
+
+#ROI_control_box = cv2.rectangle(I,(int(ROI_control[0]),int(ROI_control[1])),(int(ROI_control[0]+ROI_control[2]),int(ROI_control[1]+ROI_control[3])),(255,0,0),3)
+#ROI_test_box = cv2.rectangle(I,(int(ROI_control[0]),int(ROI_control[1])+Distance_control_test),(int(ROI_control[0]+ROI_control[2]),int(ROI_control[1]+ROI_control[3])+Distance_control_test),(0,0,255),3)
+#ROI_background_box = cv2.rectangle(I,(int(ROI_control[0]),int(ROI_control[1])+Distance_control_background),(int(ROI_control[0]+ROI_control[2]),int(ROI_control[1]+ROI_control[3])+Distance_control_background),(0,0,0),3)
+
+#plt.imshow(cv2.cvtColor(I, cv2.COLOR_BGR2RGB))
+#plt.show()
