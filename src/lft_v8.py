eb93bed5ed33429895b6b7103d772e8422dc165c
Koji Abe <Koji@Koji-Abe-no-MacBook.local>
2019-01-02 21:08:56 -0800

    index changed to x-coordinate

--- src/lft_v8.py
+++ src/lft_v8.py
@@ -6,6 +6,8 @@ from matplotlib.cm import ScalarMappable
 
 #from mpl_toolkits.basemap import Basemap
 import pandas as pd
+from pandas import DataFrame
+
 import matplotlib as mpl
 
 from numpy import exp, loadtxt, pi, sqrt
@@ -88,9 +90,11 @@ for (i, c) in enumerate(cnts):
 
         # write ellipses from the center of the rectangles
         ellipse_control = cv2.ellipse(I, ((rect[0]+(rect[2]/2), rect[1]+(rect[3]/2)), (5, 5), -1), (255, 0, 0), 3)
-        #list = zip(ellipse_control)
-        #for i in enumerate(list):
-            #cv2.putText(I, "#{}".format(i + 1), (rect[0]+(rect[2]/2), rect[1]+(rect[3]/2) - 15), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255, 0, 0), 1)
+
+        # add number for each rectangle
+        coordinate = tuple( sorted(centerCoord) )
+        #print (coordinate[1])
+        cv2.putText(I, "x:{}".format(coordinate[1]), (rect[0]+(rect[2]/2) - 25, rect[1]+(rect[3]/2) - 20), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255, 0, 0), 2)
 
         #Box at test line
         x1, y1 = int(rect[0]+(rect[2]/2) - (ROI_test_box_width/2)), int(rect[1]+(rect[3]/2) + Distance_control_test - (ROI_test_box_height/2))
@@ -120,7 +124,7 @@ for (i, c) in enumerate(cnts):
 
         #plot signal at test line more than 3STD of background
         plt.subplot(221)
-        plt.plot([1-x for x in myAvg] - (np.mean([1-x for x in myAvg2]) + np.mean([1-x for x in myAvg3]))/2 - (np.std([1-x for x in myAvg2]) + np.std([1-x for x in myAvg3]))/2*3, label = i+1)
+        plt.plot([1-x for x in myAvg] - (np.mean([1-x for x in myAvg2]) + np.mean([1-x for x in myAvg3]))/2 - (np.std([1-x for x in myAvg2]) + np.std([1-x for x in myAvg3]))/2*3, label = coordinate[1])
         plt.legend()
         plt.title("Profile at test line")
         plt.xlabel("Pixels")
@@ -130,9 +134,9 @@ for (i, c) in enumerate(cnts):
         results = np.trapz([1-x for x in myAvg] - (np.mean([1-x for x in myAvg2]) + np.mean([1-x for x in myAvg3]))/2 - (np.std([1-x for x in myAvg2]) + np.std([1-x for x in myAvg3]))/2*3)
         print results
         plt.subplot(222)
-        plt.scatter([i + 1], results)
+        plt.scatter([coordinate[1]], results)
         plt.title("Integrated signal intensity")
-        plt.xlabel("Sample#")
+        plt.xlabel("x-coordinate (pixel)")
         plt.ylabel("Integrated signal intensity")
 
         # write ROI at test line and background, based on the center of the control line
@@ -141,13 +145,7 @@ for (i, c) in enumerate(cnts):
         ROI_background2_box = cv2.rectangle(I,(int(rect[0]+(rect[2]/2) - (ROI_background2_box_width/2)),int(rect[1]+(rect[3]/2) + Distance_control_background2 - (ROI_background2_box_height/2))),(int(rect[0]+(rect[2]/2) + (ROI_background2_box_width/2)),int(rect[1]+(rect[3]/2) + Distance_control_background2 + (ROI_background_box_height/2))),(0,0,0),1)
 
         # add number for each rectangle
-        cv2.putText(I, "#{}".format(i + 1), (rect[0]+(rect[2]/2) - 20, rect[1]+(rect[3]/2) - 15), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2)
-        #[(count) for count in enumerate(centerCoord)]
-        #[[i,j] for i,j in enumerate(ellipse_control)]
-        #cv2.putText(I, "#{}".format(i), (rect[0]+(rect[2]/2) - 20, rect[1]+(rect[3]/2) - 15), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0), 1)
-
-
-
+        #cv2.putText(I, "#{}".format(i + 1), (rect[0]+(rect[2]/2) - 20, rect[1]+(rect[3]/2) - 15), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2)
 
         # output to text file
         #lines = ['roi_corrected_0to1', str(roi_corrected_0to1), 'roi_test_avg_intensity', str(roi_test_avg_intensity), 'roi_background_avg_intensity', str(roi_background_avg_intensity)]
@@ -159,14 +157,14 @@ for (i, c) in enumerate(cnts):
         plt.subplot(223)
         plt.imshow(cv2.cvtColor(I_resize, cv2.COLOR_BGR2RGB))
         plt.title("Raw image")
-        plt.xlabel("Pixels")
-        plt.ylabel("Pixels")
+        plt.xlabel("x-coordinate (pixel)")
+        plt.ylabel("y-coordinate (pixel)")
 
         plt.subplot(224)
         plt.imshow(cv2.cvtColor(I, cv2.COLOR_BGR2RGB))
         plt.title("ROI on image")
-        plt.xlabel("Pixels")
-        plt.ylabel("Pixels")
+        plt.xlabel("x-coordinate (pixel)")
+        plt.ylabel("y-coordinate (pixel)")
 
 
 plt.show()
